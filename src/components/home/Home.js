4fb94bbe7b568140d07ed6f0ef1679c994aeafae
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-07-09 17:38:27 +0300

    add homepage title to center

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './Home.css';
+import HomepageLogo from "../../assets/ilkyaz-logo-homepage.png";
 
 var Home = function () {
     return (
-        <div>
-            <h2>Home</h2>
+        <div className="main">
+            <img className="homepage-logo" src={HomepageLogo}></img>
         </div>
       );
 }
