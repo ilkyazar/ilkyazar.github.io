e1ea09a63aaf0718d05644200bcb0dcfd864c3bb
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-07-12 19:20:20 +0300

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-This is for my personal website. I developed it with React.
---
+++ precache-manifest.c3f04733e34506538856079db3b61e6c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f77936dfc3a920100185c02f5da9f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed622c1fc03ce6f0eefd",
+    "url": "/static/css/main.4c8c0832.chunk.css"
+  },
+  {
+    "revision": "2ca3f3648c360b0e83f9",
+    "url": "/static/js/2.0d0eac85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0d0eac85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed622c1fc03ce6f0eefd",
+    "url": "/static/js/main.18c5566b.chunk.js"
+  },
+  {
+    "revision": "6b96dd93f7fa3d5d518e",
+    "url": "/static/js/runtime-main.a20b8e7c.js"
+  },
+  {
+    "revision": "ba5e581a9e47fdfcbe05172f27620001",
+    "url": "/static/media/codepen.ba5e581a.png"
+  },
+  {
+    "revision": "63a852009aa53b664e42c8a2e4103dc0",
+    "url": "/static/media/github.63a85200.png"
+  },
+  {
+    "revision": "e52b75c51a1c3aebd1a52e11f8921ced",
+    "url": "/static/media/gmail.e52b75c5.png"
+  },
+  {
+    "revision": "2dc2d98c812b110ca0452944f091cbd3",
+    "url": "/static/media/hackerrank.2dc2d98c.png"
+  },
+  {
+    "revision": "4c7e0915111e9ef17198fb3d0539ba01",
+    "url": "/static/media/ilkyaz-title-homepage.4c7e0915.png"
+  },
+  {
+    "revision": "0979c4cd308563a2cbcd76aefd1c2a59",
+    "url": "/static/media/visit-link.0979c4cd.png"
+  }
+]);
