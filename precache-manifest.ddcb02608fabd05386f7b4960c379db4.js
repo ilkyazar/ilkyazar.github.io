849679963701ae7a1014d550e6652803a655431f
ilkyazar <ilkyaz.arabaci@gmail.com>
2020-11-08 15:02:32 +0300

    Updates

---
+++ precache-manifest.ddcb02608fabd05386f7b4960c379db4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6969d77a9d07163bd4948dac3e287c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1ebbacbfa0d99f9f715",
+    "url": "/static/css/main.2b690b83.chunk.css"
+  },
+  {
+    "revision": "33f0b9c094ddbdc6773a",
+    "url": "/static/js/2.78391b44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.78391b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ebbacbfa0d99f9f715",
+    "url": "/static/js/main.160e2be2.chunk.js"
+  },
+  {
+    "revision": "6b96dd93f7fa3d5d518e",
+    "url": "/static/js/runtime-main.a20b8e7c.js"
+  },
+  {
+    "revision": "ba5e581a9e47fdfcbe05172f27620001",
+    "url": "/static/media/codepen.ba5e581a.png"
+  },
+  {
+    "revision": "63a852009aa53b664e42c8a2e4103dc0",
+    "url": "/static/media/github.63a85200.png"
+  },
+  {
+    "revision": "e52b75c51a1c3aebd1a52e11f8921ced",
+    "url": "/static/media/gmail.e52b75c5.png"
+  },
+  {
+    "revision": "2dc2d98c812b110ca0452944f091cbd3",
+    "url": "/static/media/hackerrank.2dc2d98c.png"
+  },
+  {
+    "revision": "4c7e0915111e9ef17198fb3d0539ba01",
+    "url": "/static/media/ilkyaz-title-homepage.4c7e0915.png"
+  },
+  {
+    "revision": "0f963df1217158a1b69ef578cf756d23",
+    "url": "/static/media/ilkyaz_photo.0f963df1.JPG"
+  },
+  {
+    "revision": "0979c4cd308563a2cbcd76aefd1c2a59",
+    "url": "/static/media/visit-link.0979c4cd.png"
+  }
+]);
